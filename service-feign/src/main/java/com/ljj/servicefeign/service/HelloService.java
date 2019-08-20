8b8759a6c38ff3d013cc5931d296813f1eab6073
CCodingMan <2693986772@qq.com>
2019-08-20 17:35:09 +0800

    加入断路器功能

--- service-feign/src/main/java/com/ljj/servicefeign/service/HelloService.java
+++ service-feign/src/main/java/com/ljj/servicefeign/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.ljj.servicefeign.service;
 
+import com.ljj.servicefeign.util.HelloServiceHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Date: 2019/8/20 16:40
  * @Description:
  */
-@FeignClient("eureka-client")
+@FeignClient(value = "eureka-client", fallback = HelloServiceHystrix.class)
 public interface HelloService {
     @GetMapping("/hello/{name}")
     String hello(@PathVariable("name") String name);
